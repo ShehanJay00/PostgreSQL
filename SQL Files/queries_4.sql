aae5620d6f3e386af68a4f120f50b974438a2336
Shehan Jayasooriya <116498102+ShehanJay00@users.noreply.github.com>
2024-04-12 21:22:07 +0530

    COALESCE function

--- SQL Files/queries_4.sql
+++ SQL Files/queries_4.sql
@@ -54,9 +54,113 @@ FROM tableName;
 SELECT customer_id,
 CASE 
 	WHEN (customer_id <= 100) THEN 'Premium Customers'
-	WHEN (customer_id BETWEEN 100 AND 200) THEN 'Plus'
-	ELSE 'Normal'
+	WHEN (customer_id BETWEEN 100 AND 200) THEN 'Plus Customers'
+	ELSE 'Normal Customers'
 END
 FROM customer;
+
+--Rename the case column
+SELECT customer_id,
+CASE 
+	WHEN (customer_id <= 100) THEN 'Premium Customers'
+	WHEN (customer_id BETWEEN 100 AND 200) THEN 'Plus Customers'
+	ELSE 'Normal Customers'
+END AS customer_class
+FROM customer;
+
+
+--CASE expression
+SELECT customer_id,
+CASE customer_id
+	WHEN 2 THEN  'Winner'
+	WHEN 5 THEN  'Second Place'
+	ELSE 'Normal'
+END AS raffle_results
+FROM customer
+ORDER BY customer_id;
+
+
+SELECT
+CASE rental_rate
+	WHEN 0.99 THEN 1
+	ELSE 0
+END
+FROM film;
+--Only shows the case column
+
+SELECT rental_rate,
+CASE rental_rate
+	WHEN 0.99 THEN 1
+	ELSE 0
+END
+FROM film;
+
+
+--Count how many 99 cent movies have
+SELECT
+SUM(CASE rental_rate
+	WHEN 0.99 THEN 1
+	ELSE 0
+END) AS number_of_bargains,
+
+SUM(CASE rental_rate
+	WHEN 2.99 THEN 1
+    ELSE 0
+END) number_of_regular
+FROM film;
+
+
+SELECT
+SUM(CASE rating
+	WHEN 'R'  THEN 1
+	ELSE 0
+END) AS r  ,
+
+SUM(CASE rating
+	WHEN 'PG' THEN 1
+    ELSE 0
+END) AS pg,
+
+SUM(CASE rating
+	WHEN 'PG-13' THEN 1
+    ELSE 0
+END) AS pg13
+FROM film;
+
+
+
+
+
+
+/*
+			COALESCE function
+			
+Accepts an unlimited number of arguments
+and returns the first argument that is NOT NULL
+
+COALESCE(arg_1 , arg_2 , arg_3 , arg_4)
+
+Usefull when querying a table that contains null values
+and substituting it with another value
+
+SELECT item , (price - COALESCE(discount , 0))
+AS final_price 
+FROM table 
+
+First check discount and check it is a Non-Null value
+If its a non null it returns the value
+If this count is NULL then it is assigned the 0 
+(the next value of the COALESCE function)
+
+
+*/
+
+
+
+
+
+
+
+
 
 
