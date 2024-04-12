1a0d83f15a1b7511b68f0dbe78d88026f0c6f41c
Shehan Jayasooriya <116498102+ShehanJay00@users.noreply.github.com>
2024-04-12 22:20:28 +0530

    NULLIF

---
+++ SQL Files/testme.sql
@@ -0,0 +1,11 @@
+/*
+						NULL IF function
+Takes 2 inputs and returns NULL if both equal , otherwise it
+returns the FIRST ARGUMENT passed.
+
+NULLIF(10,10)
+returns NULL
+
+NULLIF(10,12)
+returns 10
+*/
